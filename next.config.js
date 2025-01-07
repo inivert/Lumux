6e9654c877cc8d41342bcb0f98f8abd8978c5318
inivert <inivert@duck.com>
2025-01-07 00:51:34 -0500

    Update Next.js configuration: enable React strict mode, streamline image domains, and adjust Webpack configuration for crypto module resolution.

--- next.config.js
+++ next.config.js
@@ -1,40 +1,19 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+	reactStrictMode: true,
 	images: {
-		domains: ["localhost"],
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cdn.sanity.io",
-				port: "",
-			},
-			{
-				protocol: "https",
-				hostname: "lh3.googleusercontent.com",
-				port: "",
-			},
-			{
-				protocol: "https",
-				hostname: "avatars.githubusercontent.com",
-				port: "",
-			},
-			{
-				protocol: "https",
-				hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-				port: "",
-			},
+		domains: [
+			"avatars.githubusercontent.com",
+			"lh3.googleusercontent.com",
+			"res.cloudinary.com",
+			"utfs.io",
 		],
 	},
 	webpack: (config) => {
-		config.resolve.fallback = {
-			fs: false,
-			net: false,
-			tls: false,
-			crypto: require.resolve('crypto-browserify'),
-		};
+		config.resolve.alias.crypto = require.resolve('crypto-browserify');
 		return config;
-	}
+	},
 };
 
 module.exports = nextConfig;
